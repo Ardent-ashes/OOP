a4d77b9d4b1c6bb1d37441a27b74bd6448da0912
Ardent-ashes <128883807+Ardent-ashes@users.noreply.github.com>
2024-02-01 04:03:52 +0600

    array string prac

---
+++ prac.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class prac{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // // Keep reading input until the end of file (EOF) is reached
+        // while (scanner.hasNextLine()) {
+        //     // Read the next line of input
+        //     String line = scanner.nextLine();
+            
+        //     // Process the input line
+        //     System.out.println("Input line: " + line);
+        // }
+
+
+        // String s=scanner.nextLine();
+       
+        // s="q"+s.substring(1);
+        // char a=s.charAt(0);
+       
+
+        //int ara[]=new int[5];
+        int ara[]={0,1,2,3,4};
+       System.out.println(ara[1]);
+        
+        scanner.close();
+    }
+}
